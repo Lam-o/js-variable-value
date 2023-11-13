98046a9b7acfe9b14bb890bf235f35b156f4c38b
lam <i.mailed.lam@gmail.com>
2023-11-13 15:28:15 +0200

    VAr and values

---
+++ script.js
@@ -0,0 +1,10 @@
+var twelveString = "12";
+var twelveNum = 12;
+
+if (twelveString === twelveNum) {
+  console.log("Yep, these two variable valuesand types are the same.");
+} else if (twelveString === twelveNum) {
+  console.log("The values of these two variables are the same.");
+} else {
+  console.log("Not sure .. but something went wrong");
+}
